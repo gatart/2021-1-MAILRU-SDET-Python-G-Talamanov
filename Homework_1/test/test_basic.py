e7cf3341edbd8d4da3954462e4e5db9d007e6a82
gatart <talamanov01@mail.ru>
2021-04-08 07:24:26 +0300

    Remove skips, took out the hardcode

--- Homework_1/test/test_basic.py
+++ Homework_1/test/test_basic.py
@@ -7,14 +7,12 @@ from base import BaseCase
 
 @pytest.mark.UI
 class TestAuth(BaseCase):
-    @pytest.mark.skip(reason='no need')
     def test_good_auth(self):
         self.auth_page.auth('talamanov01@mail.ru', "qwerty123456")
         wait = WebDriverWait(self.driver, timeout=20)
         wait.until(EC.presence_of_element_located(locators.AuthPageLocators.AUTH_LOCATOR))
         wait.until(EC.visibility_of_element_located(locators.AuthPageLocators.AUTH_LOCATOR))
 
-    @pytest.mark.skip(reason='no need')
     def test_logout(self, auto_auth):
         self.driver = auto_auth
         self.auth_page.logout()
@@ -23,7 +21,6 @@ class TestAuth(BaseCase):
 
 @pytest.mark.UI
 class TestGoTo(BaseCase):
-    # @pytest.mark.skip(reason='no need')
     @pytest.mark.parametrize("checks",
                              ((locators.SegmentLocators.SEGMENT_LOCATOR, locators.SegmentLocators.SEGMENT_CHECK_1),
                               (locators.ProfileLocators.PROF_LOCATOR, locators.ProfileLocators.FIO_LOCATOR)))
@@ -35,7 +32,6 @@ class TestGoTo(BaseCase):
 
 @pytest.mark.UI
 class TestProfile(BaseCase):
-    @pytest.mark.skip(reason='no need')
     def test_change(self, auto_auth):
         self.driver = auto_auth
         self.prof_page.change()
--- Homework_1/ui/pages/profile_page.py
+++ Homework_1/ui/pages/profile_page.py
@@ -3,36 +3,55 @@ from ui.pages.auth_page import AuthPage
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 class ProfPage(AuthPage):
     def change(self):
-        self.click(locators.ProfileLocators.PROF_LOCATOR, 20)
+        self.profile()
         self.input("George", locators.ProfileLocators.FIO_LOCATOR)
         self.input("+77777777777", locators.ProfileLocators.NUM_LOCATOR)
         element = self.find(locators.ProfileLocators.EMAIL_BUTTON)
         element.click()
         self.input("hahaha@mail.ru", locators.ProfileLocators.EMAIL_LOCATOR)
-        element = self.find(locators.ProfileLocators.SAFE_BUTTON)
+        element = self.save_but()
         element.click()
-        self.click(locators.ProfileLocators.BACK_LINK)
+        self.back()
 
     def check(self):
-        self.click(locators.ProfileLocators.PROF_LOCATOR, 20)
-        input_el = self.find(locators.ProfileLocators.FIO_LOCATOR)
-        assert "George" == input_el.get_attribute('value')
-        input_el = self.find(locators.ProfileLocators.NUM_LOCATOR)
-        assert "+77777777777" == input_el.get_attribute('value')
-        input_el = self.find(locators.ProfileLocators.EMAIL_LOCATOR)
-        assert "hahaha@mail.ru" == input_el.get_attribute('value')
+        self.profile()
+        assert "George" == self.value(self.fio())
+        assert "+77777777777" == self.value(self.num())
+        assert "hahaha@mail.ru" == self.value(self.email())
 
     def clear(self):
-        field = self.find(locators.ProfileLocators.FIO_LOCATOR)
+        field = self.fio()
         field.send_keys(Keys.BACK_SPACE * 15)
-        field = self.find(locators.ProfileLocators.NUM_LOCATOR)
+        field = self.num()
         field.send_keys(Keys.BACK_SPACE * 15)
-        field = self.find(locators.ProfileLocators.EMAIL_LOCATOR)
+        field = self.email()
         field.send_keys(Keys.BACK_SPACE * 15)
-        element = self.find(locators.ProfileLocators.SAFE_BUTTON)
+        element = self.save_but()
         element.click()
         time.sleep(10)
-        self.click(locators.ProfileLocators.BACK_LINK)
+        self.back()
         time.sleep(10)
+
+    def fio(self):
+        return self.find(locators.ProfileLocators.FIO_LOCATOR)
+
+    def num(self):
+        return self.find(locators.ProfileLocators.NUM_LOCATOR)
+
+    def email(self):
+        return self.find(locators.ProfileLocators.EMAIL_LOCATOR)
+
+    def save_but(self):
+        return self.find(locators.ProfileLocators.SAFE_BUTTON)
+
+    def profile(self):
+        self.click(locators.ProfileLocators.PROF_LOCATOR, 20)
+
+    def back(self):
+        self.click(locators.ProfileLocators.BACK_LINK)
+
+    def value(self, el):
+        return el.get_attribute('value')
